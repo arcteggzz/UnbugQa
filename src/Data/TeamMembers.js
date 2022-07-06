91426ad49c733cccdacb18457653bb29d06c3fe1
Oghenetega Esedere <arcteggzz@gmail.com>
2022-07-07 00:42:47 +0100

    Created Data for Meet the team

---
+++ src/Data/TeamMembers.js
@@ -0,0 +1,40 @@
+const TeamMembersData = [
+    {
+        Name: "Alisa Hester",
+        Position: "Founder & CEO",
+        Roles: "Former co-founder of Opendoor. Early staff at Spotify and Clearbit.",
+        Image: ""
+    },
+    {
+        Name: "Rich Wilson",
+        Position: "Engineering Manager",
+        Roles: "Lead engineering teams at Figma, Pitch, and Protocol Labs.",
+        Image: ""
+    },
+    {
+        Name: "Annie Stanley",
+        Position: "Product Manager",
+        Roles: "Former PM for Airtable, Medium, Ghost, and Lumi.",
+        Image: ""
+    },
+    {
+        Name: "Alisa Hester",
+        Position: "Founder & CEO",
+        Roles: "Former co-founder of Opendoor. Early staff at Spotify and Clearbit.",
+        Image: ""
+    },
+    {
+        Name: "Rich Wilson",
+        Position: "Engineering Manager",
+        Roles: "Lead engineering teams at Figma, Pitch, and Protocol Labs.",
+        Image: ""
+    },
+    {
+        Name: "Annie Stanley",
+        Position: "Product Manager",
+        Roles: "Former PM for Airtable, Medium, Ghost, and Lumi.",
+        Image: ""
+    },
+]
+
+export default TeamMembersData
